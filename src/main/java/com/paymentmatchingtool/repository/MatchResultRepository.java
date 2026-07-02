95135c4de9cde6be52b6e8a8d0af83c2ab3be944
keshaswe-cpu <keshaswe@gmail.com>
2026-07-02 15:31:28 +0530

    changes done

--- src/main/java/com/paymentmatchingtool/repository/MatchResultRepository.java
+++ src/main/java/com/paymentmatchingtool/repository/MatchResultRepository.java
@@ -9,4 +9,6 @@ public interface MatchResultRepository extends JpaRepository<MatchResult, Long> 
     List<MatchResult> findByResolvedOrderByOrderIdAscCurrencyAsc(boolean resolved);
 
     List<MatchResult> findAllByOrderByOrderIdAscCurrencyAsc();
+
+    //repo
 }
